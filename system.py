76e5be6d0d59708ed3a2fdc5949196e6c6e01290
Yu Sun <usunyu@gmail.com>
2021-01-17 12:53:20 -0800

    [optimize] run transaction job in different thread

--- system.py
+++ system.py
@@ -2,11 +2,12 @@ import time
 import threading
 import schedule
 import pytz
-import logging
 import yfsdk
 import fmpsdk
 from datetime import datetime
 
+JOB_MAXIMUM = 100
+JOB_INTERVAL = 60  # in second
 
 watchlist = []
 positions = []
@@ -30,7 +31,7 @@ def screening_job():
     quotes.sort(key=lambda x: x["acceleration"], reverse=True)
     top_quotes = quotes[:3]
     for top_quote in top_quotes:
-        logging.info(
+        print(
             "[{}] {}, ${} ({}%)".format(
                 now,
                 top_quote["symbol"],
@@ -45,29 +46,37 @@ def screening_job():
         return schedule.CancelJob
 
 
-def transaction_job():
+def transaction_job(job_id):
     now = datetime.now()
 
-    for symbol in watchlist:
+    print("[{}] run job {}".format(now, job_id))
+
+    if job_id < len(watchlist):
+        symbol = watchlist[job_id]
         intraday_sma = fmpsdk.get_intraday_sma(symbol, "1min")
-        if len(intraday_sma) < 2:
-            continue
-        cur_sma = intraday_sma[0]
-        pre_sma = intraday_sma[1]
-        # TODO
+        if len(intraday_sma) >= 2:
+            cur_sma = intraday_sma[0]
+            pre_sma = intraday_sma[1]
+            # TODO
 
     if now.hour == 13:
         return schedule.CancelJob
 
 
-def run_threaded(job_func):
-    job_thread = threading.Thread(target=job_func)
+def run_screening_thread():
+    job_thread = threading.Thread(target=screening_job)
     job_thread.start()
 
 
+def run_transaction_thread():
+    for job_id in range(0, JOB_MAXIMUM):
+        job_thread = threading.Thread(target=transaction_job, args=(job_id,))
+        job_thread.start()
+
+
 def start_trading_job():
-    schedule.every(60).seconds.do(run_threaded, transaction_job)
-    schedule.every(60).seconds.do(run_threaded, screening_job)
+    schedule.every(JOB_INTERVAL).seconds.do(run_screening_thread)
+    schedule.every(JOB_INTERVAL).seconds.do(run_transaction_thread)
 
 
 schedule.every().monday.at("06:30").do(start_trading_job)
