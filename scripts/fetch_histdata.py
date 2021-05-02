6a6088b39806b13e1b9f1fa091432740a9f674d2
Yu Sun <usunyu@gmail.com>
2021-05-01 23:08:00 -0700

    [scripts] fetch hist candle data, II

--- scripts/fetch_histdata.py
+++ scripts/fetch_histdata.py
@@ -3,7 +3,8 @@
 # fetch minute/daily historical candle data into database
 
 def start():
-    from datetime import datetime
+    import time
+    from datetime import date
     from sdk import webullsdk
     from scripts import utils
     from old_ross.models import TradingSettings, WebullOrder
@@ -16,8 +17,8 @@ def start():
     webullsdk.login(paper=trading_settings.paper)
 
     # TODO
-    # today = datetime.today()
-    today = datetime(2021, 4, 30)
+    # today = date.today()
+    today = date(2021, 4, 30)
     # get all symbols orders in today's orders
     symbol_list = []
     ticker_id_list = []
@@ -34,15 +35,37 @@ def start():
     for i in range(0, len(symbol_list)):
         symbol = symbol_list[i]
         ticker_id = ticker_id_list[i]
-        timestamp = None
+        timestamp = int(time.time())
         candle_list = []
-        while True:
+        while timestamp:
             finish = False
             temp_candle_list = []
-            bars = webullsdk.get_1m_bars(ticker_id=ticker_id, count=500)
+            bars = webullsdk.get_1m_bars(
+                ticker_id=ticker_id, count=500, timeStamp=timestamp)
             for index, bar in bars.iterrows():
-                datetime = index.to_pydatetime()
-                # TODO
+                date_time = index.to_pydatetime()
+                if today != date_time.date():
+                    finish = True
+                else:
+                    temp_candle_list.append({
+                        'symbol': symbol,
+                        'date': date_time.date(),
+                        'time': date_time,
+                        'open': bar['open'],
+                        'high': bar['high'],
+                        'low': bar['low'],
+                        'close': bar['close'],
+                        'volume': bar['volume'],
+                        'vwap': bar['vwap'],
+                    })
+            candle_list = temp_candle_list + candle_list
+            temp_candle_list = []
+            if finish:
+                timestamp = None
+            else:
+                timestamp = int(bars.index[0].timestamp()) - 1
+        # sleep for a while
+        time.sleep(5)
 
 
 if __name__ == "django.core.management.commands.shell":
