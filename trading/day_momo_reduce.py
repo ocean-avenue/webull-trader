d011658c8c6aeace542446b8ab5ecfc1f1d9246b
Yu Sun <usunyu@gmail.com>
2021-05-27 14:59:07 -0700

    [trading][momo] revert sell / buy timeout

--- trading/day_momo_reduce.py
+++ trading/day_momo_reduce.py
@@ -25,9 +25,9 @@ class DayTradingMomoReduceSize(DayTradingMomo):
         return buy_position_amount
 
     def check_if_track_symbol(self, symbol):
-        # check if sell not long ago
-        if symbol in self.trading_stats and (datetime.now() - self.trading_stats[symbol]['last_trade_time']) <= timedelta(seconds=100):
-            return False
+        # # check if sell not long ago
+        # if symbol in self.trading_stats and (datetime.now() - self.trading_stats[symbol]['last_trade_time']) <= timedelta(seconds=100):
+        #     return False
         # check if 3 continues loss trades and still in blacklist time
         if symbol in self.trading_stats and self.trading_stats[symbol]['continue_lose_trades'] >= 3 and (datetime.now() - self.trading_stats[symbol]['last_trade_time']) <= timedelta(seconds=self.blacklist_timeout_in_sec):
             return False
