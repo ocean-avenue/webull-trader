5b766f80cefc4858608364b6f00bcd55a099c33c
Quanturtle <73768541+quanturtleteam@users.noreply.github.com>
2021-06-28 19:32:50 -0700

    Update SWING_WATCHLIST_SYMBOLS

--- scripts/config.py
+++ scripts/config.py
@@ -23,7 +23,7 @@ SWING_WATCHLIST_SYMBOLS = [
     "SPY", "SQ", "SHOP", "SNOW", "SE", "SKLZ", "SNAP", "SPOT", "SPCE", "SAVE",
     "SPG", "SPLK", "SNPS", "STXB", "SUMO", "STNE", "STPK", "SENS", "SQSP",
     "T", "TSLA", "TSM", "TWTR", "TWLO", "TDOC", "TWOU", "TLRY", "TREE", "TMUS",
-    "TOL", "TAL", "TME", "TALO", "TSN",
+    "TOL", "TAL", "TME", "TALO", "TSN", "TAN",
     "U", "UBER", "UPWK", "UAL", "UMC", "UEC",
     "V", "VIPS",
     "WMT", "WFC", "WTI", "WBA", "WISH",
