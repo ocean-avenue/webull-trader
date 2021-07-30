b9cbd3f24a69e817082ee43f598cb1465faa9d30
Yu Sun <usunyu@gmail.com>
2021-07-30 09:25:56 -0700

    [breakout] stop tracking after sell

--- trading/day_breakout.py
+++ trading/day_breakout.py
@@ -204,8 +204,7 @@ class DayTradingBreakout(StrategyBase):
             return
 
         if ticker['pending_sell']:
-            self.check_sell_order_filled(
-                ticker, resubmit_count=50, stop_tracking=False)
+            self.check_sell_order_filled(ticker, resubmit_count=50)
             return
 
         holding_quantity = ticker['positions']
