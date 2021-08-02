0970bfdda7ac3a4e0e6ed1ebe38b7b9b49c4c1be
Yu Sun <usunyu@gmail.com>
2021-08-02 12:21:51 -0700

    [scheduler] update all hour const

--- webull_trader/management/commands/runscheduler.py
+++ webull_trader/management/commands/runscheduler.py
@@ -11,7 +11,7 @@ from scripts import fetch_account, fetch_quotes, fetch_orders, fetch_news, fetch
 from trading import trading_executor
 
 WEEKDAYS = ["mon", "tue", "wed", "thu", "fri"]
-ALL_HOURS = ["05", "06", "07", "08", "09", "10", "11",
+ALL_HOURS = ["04", "05", "06", "07", "08", "09", "10", "11",
              "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 REGULAR_HOURS = ["10", "11", "12", "13", "14", "15", "16"]
 
