d06824f707644d2d9662a27d48c06ac8a50e22eb
Yu Sun <usunyu@gmail.com>
2021-08-28 10:17:57 -0700

    [trading] check peak with up percentage

--- scripts/utils.py
+++ scripts/utils.py
@@ -554,6 +554,9 @@ def check_bars_at_peak(bars, long_period=10, short_period=3):
     # prev_bar3 should be green
     if prev_bar3['close'] < prev_bar3['open']:
         return False
+    # prev_bar3 should has enough up percentage (5%)
+    if (prev_bar3['close'] - prev_bar3['open']) / prev_bar3['open'] < 0.05:
+        return False
     # prev_bar3 vol should > prev_bar2 vol
     if prev_bar3['volume'] < prev_bar2['volume']:
         return False
