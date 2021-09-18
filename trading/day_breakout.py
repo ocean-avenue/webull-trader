d56a01273c5aed9188c8a1ccf35ddc70d2130772
Yu Sun <usunyu@gmail.com>
2021-09-17 22:20:26 -0700

    [breakout] stop check chart is at peak

--- trading/day_breakout.py
+++ trading/day_breakout.py
@@ -176,11 +176,11 @@ class DayTradingBreakout(StrategyBase):
             exit_trading = True
             exit_note = "Candle chart is not volatility."
         # check if bar chart is at peak
-        elif utils.check_bars_at_peak(bars):
-            utils.print_trading_log(
-                "<{}> candle chart is at peak, exit!".format(symbol))
-            exit_trading = True
-            exit_note = "Candle chart is at peak."
+        # elif utils.check_bars_at_peak(bars):
+        #     utils.print_trading_log(
+        #         "<{}> candle chart is at peak, exit!".format(symbol))
+        #     exit_trading = True
+        #     exit_note = "Candle chart is at peak."
         elif self.check_if_trade_period_timeout(ticker):
             utils.print_trading_log(
                 "<{}> trading period timeout, exit!".format(symbol))
