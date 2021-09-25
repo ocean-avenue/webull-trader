188ae8980473020928f07702e386f7ae667abb57
Yu Sun <usunyu@gmail.com>
2021-09-25 11:55:08 -0700

    [migrate] fill min usable cash script

---
+++ migrate/min_usable_cash.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# fill webull account statistics with min usable cash
+
+
+def start():
+    from webull_trader.models import WebullAccountStatistics
+    from scripts import utils
+
+    acc_stats = WebullAccountStatistics.objects.all()
+
+    for acc_stat in acc_stats:
+        acc_stat.min_usable_cash = round(acc_stat.net_liquidation / 2)
+        acc_stat.save()
+
+    print("[{}] Migrate {} webull account statistics min usable cash done".format(
+        utils.get_now(), len(acc_stats)))
+
+
+if __name__ == "django.core.management.commands.shell":
+    start()
