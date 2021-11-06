444ff5b1bea5c8627feb801aa94a0e1c4b7852e4
Yu Sun <usunyu@gmail.com>
2021-11-05 19:27:01 -0700

    [strategy] set stop loss tight

--- trading/strategy/day_breakout.py
+++ trading/strategy/day_breakout.py
@@ -278,23 +278,10 @@ class DayTradingBreakout(StrategyBase):
 
     def get_stop_loss_price(self, bars: pd.DataFrame) -> float:
         current_candle = bars.iloc[-1]
-        prev_candle = bars.iloc[-2]
-        # current_price = current_candle['close']
-        # use max( min( prev candle middle, buy price -2% ), buy price -5% )
-        # return max(
-        #     min(round((prev_candle['high'] + prev_candle['low']) / 2, 2),
-        #         round(current_price * (1 - config.MIN_DAY_STOP_LOSS), 2)),
-        #     round(current_price * (1 - config.MAX_DAY_STOP_LOSS), 2))
-        stop_loss_1 = current_candle['low']
-        stop_loss_2 = prev_candle['low']
-        # if candle already up more than 10%
+        stop_loss = current_candle['low']
         if (current_candle['high'] - current_candle['low']) / current_candle['low'] > 0.1:
-            stop_loss_1 = round(
-                (current_candle['high'] + current_candle['low'] / 2), 2)
-        if (prev_candle['high'] - prev_candle['low']) / prev_candle['low'] > 0.1:
-            stop_loss_2 = round(
-                (prev_candle['high'] + prev_candle['low']) / 2, 2)
-        return min(stop_loss_1, stop_loss_2)
+            stop_loss = round(current_candle['low'] + (current_candle['high'] - current_candle['low']) * 0.4, 2)
+        return stop_loss
 
     def get_price_rate_of_change(self, bars: pd.DataFrame, period: int = 10) -> float:
         period = min(len(bars) - 1, period)
